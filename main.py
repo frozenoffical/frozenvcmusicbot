d6120ba4b8d0ac8665861635694e1c097afc89f9
frozenoffical <tenopno@gmail.com>
2025-12-01 14:36:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -378,7 +378,7 @@ async def start_handler(_, message):
     support_group = os.getenv("SUPPORT_GROUP", "https://t.me/Frozensupport1")
     start_animation = os.getenv(
         "START_ANIMATION",
-        "https://frozen-imageapi.lagendplayersyt.workers.dev/file/2e483e17-05cb-45e2-b166-1ea476ce9521.mp4"
+        "https://files.catbox.moe/oniqze.mp4"
     )
 
     caption = (
